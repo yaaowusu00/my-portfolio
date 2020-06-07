ec27c348247f2ceb784b37f9e8329edfe984d651
Yaa <yaaowusu00@gmail.com>
2020-06-07 09:14:39 -0400

    make small changes, deploy live site

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -6,13 +6,22 @@ const calculateAboutHeight = () => {
     $('#aboutSection').css('height', aboutHeight);
 };
 /*displays pop up on projects page when you click project picture*/
-const openTab = tabName => {
+const openTab = (tabName) => {
+    if (tabName !== "EMPTY_NAME") {
+         $(".content").css({"z-index":"3"});
+    }
+    let position = $(window).scrollTop() || (window.pageYOffset);
     const elements = Array.from(document.getElementsByClassName("descriptionContainer"));
+    elements.forEach(description => ($(".content").css({"top": ((tabName === "EMPTY_NAME") ? "0": (position + 60))})));
+    elements.forEach(description => ($('#' + tabName).css({"top": ((tabName === "EMPTY_NAME") ? "0": (position + 60))})));
     elements.forEach(description => (description.style.height = description.id === tabName ? "90vh" : "0"));
-    elements.forEach(description => (description.style.padding = description.id === tabName ? "2vh 0" : "0")); 
+    elements.forEach(description => (description.style.padding = description.id === tabName ? "2% 3%" : "0"));
 };  
 
-const closeTab = () => openTab("EMPTY_NAME");   
+const closeTab = (tabName) => {
+    openTab(tabName, "close"); 
+    $(".content").css({"z-index":"-3"});
+};
 
 $(document).ready(function(){
     let navHeight = $('.navBar').height();
